027c5588438320881f0763c19bac8ff4ca191bc2
MazeR <mazer0701@gmail.com>
2019-10-06 01:04:42 +0800

    first commit

---
+++ js/navigation.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementsByClassName("navigation-icon")[0].addEventListener("click", function() {
+        this.classList.toggle('navigation-icon-x');
+        document.getElementsByClassName("navigation")[0].classList.toggle('navigationOpen');
+    });
+}); 
